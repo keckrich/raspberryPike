8447ab26cecea79400b6e64f40f9175d4e7748a8
Kyle E <keckrich@iastate.edu>
2018-10-07 10:36:42 -0500

    fix

--- newapp/app.py
+++ newapp/app.py
@@ -47,5 +47,5 @@ if __name__ == "__main__":
 	recording_on = Value('b', True)
 	p = Process(target=record_loop, args=(recording_on,))
 	p.start()  
-	app.run(debug=True, use_reloader=False)
+	app.run
 	p.join()
