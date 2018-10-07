f567efa664b3fd2879a12f13db458f045910552e
Kyle E <keckrich@iastate.edu>
2018-10-07 11:24:00 -0500

    uft

--- app/app.py
+++ app/app.py
@@ -40,9 +40,8 @@ def data():
 	global i
 	rpm =  i/16.77
 	mph = i * 4.65
-	i = 0
 	return jsonify({'titlejson' : 'ITS WORKING!!!!', 'currentSpeedjson' : mph, 'sessionDistance': '2', 'timeLeftjson' : '1.23', 'SessionDuration': '2','rotationsperminute': '312', 'DistaceTotal': '23', 'TimeTotal': '13', 'DistaceTraveled': '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
-
+        i = 0
 	
 	
 def record_loop(loop_on):
