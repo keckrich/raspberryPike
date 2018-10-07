a90fef28332f08c33b7daf61f1cf38f954280707
Kyle E <keckrich@iastate.edu>
2018-10-07 12:49:13 -0500

    as

--- app/app.py
+++ app/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify
 from random import sample
-
+from random import randint
 i = 0 
 
 app = Flask(__name__)
@@ -17,7 +17,7 @@ def index():
 def data():
 	global i
 	i += 1
-	x = sample(range(4,10), 1)
+	x = randint(5, 10)
 
 	return jsonify({'titlejson' : 'rasberry Pike', 'currentSpeedjson' : str(x), 'sessionDistance': str(i), 'timeLeftjson' : '1.23', 'SessionDuration': str(i),'rotationsperminute': '312', 'DistaceTotal': str(i * .23), 'TimeTotal': '13', 'DistaceTraveled' : '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
 
