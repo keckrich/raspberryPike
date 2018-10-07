14d236e9d2d854c2061261b3667f84b2db487e06
Jonathan <41304994+Jonathannsegal@users.noreply.github.com>
2018-10-07 10:48:48 -0500

    update

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, jsonify
+from random import sample
 
 app = Flask(__name__)
 @app.route('/<path:path>')
@@ -11,7 +12,7 @@ def index():
 
 @app.route('/data')
 def data():
-    return jsonify({'titlejson' : 'ITS WORKING!!!!', 'currentSpeedjson' : '12.3', 'timeLeftjson' : '1.23'})
+    return jsonify({'titlejson' : 'ITS WORKING!!!!', 'currentSpeedjson' : sample(range(1,10), 1), 'sessionDistance': '2', 'timeLeftjson' : '1.23', 'SessionDuration': '2','rotationsperminute': '312', 'DistaceTotal': '23', 'TimeTotal': '13', 'DistaceTraveled': '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
 
 if __name__ == '__main__':
     app.run()
