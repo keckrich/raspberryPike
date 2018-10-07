e319f17655aea45a2b82baef897d21fae11916c9
Kyle E <keckrich@iastate.edu>
2018-10-07 12:46:22 -0500

    asd

--- app/app.py
+++ app/app.py
@@ -18,8 +18,8 @@ def data():
 	global i
 	i += 1
 	x = sample(range(4,10), 1)
-	
-    return jsonify({'titlejson' : 'rasberry Pike', 'currentSpeedjson' : str(x), 'sessionDistance': str(i), 'timeLeftjson' : '1.23', 'SessionDuration': str(i),'rotationsperminute': '312', 'DistaceTotal': str(i * .23), 'TimeTotal': '13', 'DistaceTraveled' : '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
+
+	return jsonify({'titlejson' : 'rasberry Pike', 'currentSpeedjson' : str(x), 'sessionDistance': str(i), 'timeLeftjson' : '1.23', 'SessionDuration': str(i),'rotationsperminute': '312', 'DistaceTotal': str(i * .23), 'TimeTotal': '13', 'DistaceTraveled' : '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
 
 if __name__ == '__main__':
     app.run()
