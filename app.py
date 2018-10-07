a9795d46d3996fe8127f66f7fb912885a88d2471
Kyle Eckrich <keckrich@iastate.edu>
2018-10-07 03:19:11 +0100

    py app

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/data')
 def hello():
-    return "Hello World!"
+    
+  #  return render_template('script.js', data = data) 
+    #return "Hello World!"
+    
+    return ('{"name":"John", "age":31, "city":"New York"}')
 
 if __name__ == '__main__':
     app.run()
