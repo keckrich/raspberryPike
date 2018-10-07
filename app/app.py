e2f352596158ed0ebb2b85550af3615b3023e652
Jonathan <41304994+Jonathannsegal@users.noreply.github.com>
2018-10-07 10:51:44 -0500

    update

--- app/app.py
+++ app/app.py
@@ -12,7 +12,7 @@ def index():
 
 @app.route('/data')
 def data():
-    return jsonify({'titlejson' : 'ITS WORKING!!!!', 'currentSpeedjson' : sample(range(1,10), 1), 'sessionDistance': '2', 'timeLeftjson' : '1.23', 'SessionDuration': '2','rotationsperminute': '312', 'DistaceTotal': '23', 'TimeTotal': '13', 'DistaceTraveled': '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
+    return jsonify({'titlejson' : 'ITS WORKING!!!!', 'currentSpeedjson' : sample(range(1,10), 1), 'sessionDistance': '2', 'timeLeftjson' : '1.23', 'SessionDuration': '2','rotationsperminute': '312', 'DistaceTotal': '23', 'TimeTotal': '13', 'DistaceTraveled' : '1 mile', 'AverageSpeed': '15', 'InternetLeft': '21', 'bitsUsing' : '1234', 'bitsMaking' : '42', 'ranking' : 'winning', 'nextranking' : 'maybe winning', 'session': '5', 'AverageInternet': '23'})
 
 if __name__ == '__main__':
     app.run()
