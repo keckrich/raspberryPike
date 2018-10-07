e9c5ef60264f69d8cc6376e80ef82f6d117513b4
Jonathan <41304994+Jonathannsegal@users.noreply.github.com>
2018-10-06 19:40:10 -0500

    add preloader for site and minor updates

--- gui/script.js
+++ gui/script.js
@@ -1,5 +1,18 @@
 window.onload = function start() {
   // httpGet("url of your python function");
+  $("html, body").animate({ scrollTop: $('#preloader').offset().top }, 10);
+  $("html, body").delay(8000).animate({scrollTop: $('#mainscreen').offset().top }, 1000);
+
+  var lineDrawing = anime({
+    targets: '#lineDrawing .lines path',
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: 'easeInOutSine',
+    duration: 1500,
+    delay: function(el, i) { return i * 250 },
+    direction: 'alternate',
+    loop: true
+  });
+
   document.getElementById('currentspeed').innerHTML =
 "23.9";
 document.getElementById('currentime').innerHTML =
